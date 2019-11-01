c42274e2795ed07c975c15fa1befa8647d0ab71a
Fatuma <fingabire15@alustudent.com>
2019-11-01 17:44:16 +0400

    JS for small screen sidenav bar

--- js/ui.js
+++ js/ui.js
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', function(){
     //nav menu
     const menus = document.querySelectorAll('.side-menu');
     M.Sidenav.init(menus, {edge: 'right'});
-});
+}); 
