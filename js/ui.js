41a4c1fd53e701a57e931a6c80baa45c15801ae2
Fatuma <fingabire15@alustudent.com>
2019-11-07 09:57:41 +0400

    Service Worker

--- js/ui.js
+++ js/ui.js
@@ -3,13 +3,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const menus = document.querySelectorAll('.side-menu');
     M.Sidenav.init(menus, {edge: 'right'});
     
-//    //dropdown
-//    var triggers = document.querySelectorAll('.dropdown-trigger');
-//    
-//    var options = {
-//        'closeOnClick': true,
-//        'hover':true
-//    }
-//    
-//    var instances = M.Dropdown.init(triggers, options);
+    //collapsible
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+
 }); 
